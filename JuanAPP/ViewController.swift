a0eb91fc9fc5a595eec6f6a8ee753c80c0413f4f
Edgar Ortiz Loyola <virtualizacion05@virtualizacions-imac-2.local>
2018-08-16 19:52:53 -0500

    Initial Commit

---
+++ JuanAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuanAPP
+//
+//  Created by Virtualizacion 05 on 8/16/18.
+//  Copyright © 2018 Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
