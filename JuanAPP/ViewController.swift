235623c40984b3b625181f6f86ef57a4d8bfceff
Virtualizacion 05 <virtualizacion05@Virtualizacions-iMac-2.local>
2018-08-22 19:14:02 -0500

    First app

--- JuanAPP/ViewController.swift
+++ JuanAPP/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
+//clase nombre de clase:super clase
 class ViewController: UIViewController {
-
+    //declaraciones de las propiedades de esta clase
+    @IBOutlet weak var labelDePregunta: UILabel!
+    @IBOutlet weak var labelDeRespuesta: UILabel!
+    
+    let preguntas : Array<String> = ["¿Cuanto es dos mas dos?","Capital de Bukina Faso","¿Como se apellida juan?"];
+    var respuestas=["Cuatro","Ouagadougou","Topo"];
+    var preguntaActual = 0;
+    
+    //metodos
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //demasiada memoria
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+//    @IBAction func botonAplastado(_ sender: UIButton) {
+//        print("Hola Mundo!");
+//    }
+    @IBAction func siguientePregunta() {
+        self.preguntaActual += 1;
+        if (self.preguntaActual == self.preguntas.count){
+            self.preguntaActual = 0;
+        }
+        self.labelDePregunta.text = self.preguntas[preguntaActual];
+        self.labelDeRespuesta.text = "¿?";
+    }
+    
+    @IBAction func darRespuesta() {
+        self.labelDeRespuesta.text = self.respuestas[preguntaActual];
+    }
 }
 
