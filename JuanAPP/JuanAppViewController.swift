fac037a5fab7b02a9d211fa0cc56702039e04686
Virtualizacion 11 <virtualizacion11@Virtualizacions-iMac-7.local>
2018-09-19 17:40:59 -0500

    Add wrong and right answer

--- JuanAPP/JuanAppViewController.swift
+++ JuanAPP/JuanAppViewController.swift
@@ -50,7 +50,8 @@ class JuanAppViewController: UIViewController {
     @IBOutlet weak var botonAtras: UIButton!
     @IBOutlet weak var botonSiguiente: UIButton!
     
-    @IBOutlet weak var labelPreguntas: UILabel!
+	@IBOutlet weak var labelMensaje: UILabel!
+	@IBOutlet weak var labelPreguntas: UILabel!
     @IBOutlet weak var labelCategoria: UILabel!
     
     @IBOutlet weak var botonRespuesta1: UIButton!
@@ -59,6 +60,7 @@ class JuanAppViewController: UIViewController {
     @IBOutlet weak var botonRespuesta4: UIButton!
     
     let modeloQuiz = Quiz();
+    var categoria = -1;
     
     
     
@@ -68,6 +70,10 @@ class JuanAppViewController: UIViewController {
         botonAtras.isHidden = true;
         botonSiguiente.isHidden = true;
         labelCategoria.isHidden = true;
+        botonRespuesta1.isHidden = true;
+        botonRespuesta2.isHidden = true;
+        botonRespuesta3.isHidden = true;
+        botonRespuesta4.isHidden = true;
         // Do any additional setup after loading the view, typically from a nib.
     }
     //demasiada memoria
@@ -89,7 +95,10 @@ class JuanAppViewController: UIViewController {
         switch sender.tag {
         case 0:
             print("1");
+            categoria = 0;
             labelPreguntas.text = self.modeloQuiz.preguntas(numeroDeCategoria: 0);
+            estiloBoton();
+            mostrarEnBotones();
         case 1:
             print("2");
         case 2:
@@ -104,13 +113,28 @@ class JuanAppViewController: UIViewController {
         for j in 6...7{
             self.view.subviews[j].isHidden=false;
         }
-//        botonAtras.isEnabled = false;
-//        botonSiguiente.isEnabled = false;
+        botonAtras.isEnabled = false;
+        botonSiguiente.isEnabled = false;
         labelCategoria.isHidden = false;
         labelCategoria.text = sender.currentTitle;
         for i in 1...5{
             self.view.subviews[i].isHidden = true;
         }
+        botonRespuesta1.isHidden = false;
+        botonRespuesta2.isHidden = false;
+        botonRespuesta3.isHidden = false;
+        botonRespuesta4.isHidden = false;
+    }
+    
+    @IBAction func siguienteCategoria() {
+            categoria = 0;
+            labelPreguntas.text = self.modeloQuiz.preguntas(numeroDeCategoria: categoria);
+            estiloBoton();
+        mostrarEnBotones();
+        desblockeaBotones();
+        botonSiguiente.isEnabled = false;
+        botonAtras.isEnabled = false;
+		labelMensaje.text = "";
     }
     
     @IBAction func regresaCategorias() {
@@ -121,6 +145,73 @@ class JuanAppViewController: UIViewController {
             self.view.subviews[j].isHidden=true;
         }
         labelCategoria.isHidden = true;
+        botonRespuesta1.isHidden = true;
+        botonRespuesta2.isHidden = true;
+        botonRespuesta3.isHidden = true;
+        botonRespuesta4.isHidden = true;
+        labelPreguntas.text = "Elige una categoría";
+		labelMensaje.text = "";
+		desblockeaBotones();
+    }
+    
+    @IBAction func BotonRespuestaCorrecta(_ sender: Any) {
+        botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+        botonAtras.isEnabled = true;
+        botonSiguiente.isEnabled = true;
+        blockeaBotones();
+		labelMensaje.text = "¡Correcto!";
+    }
+	
+	
+	@IBAction func BotonRespuestaIncorrecta(_ sender: UIButton) {
+		switch sender.tag {
+		case 5:
+		botonRespuesta2.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+		case 6:
+		botonRespuesta3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+		case 7:
+		botonRespuesta4.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+		default:
+			print("Esto no pasara");
+		}
+		botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+		blockeaBotones();
+		labelMensaje.text = "¡Incorrecto!";
+		botonAtras.isEnabled = true;
+		botonSiguiente.isEnabled = true;
+	}
+	
+    
+    func estiloBoton(){
+        botonRespuesta1.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2);
+        botonRespuesta1.borderWidth = 3;
+        botonRespuesta2.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2);
+        botonRespuesta2.borderWidth = 3;
+        botonRespuesta3.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2);
+        botonRespuesta3.borderWidth = 3;
+        botonRespuesta4.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2);
+        botonRespuesta4.borderWidth = 3;
+    }
+    
+    func blockeaBotones(){
+        botonRespuesta1.isEnabled = false;
+        botonRespuesta2.isEnabled = false;
+        botonRespuesta3.isEnabled = false;
+        botonRespuesta4.isEnabled = false;
+    }
+    
+    func desblockeaBotones(){
+        botonRespuesta1.isEnabled = true;
+        botonRespuesta2.isEnabled = true;
+        botonRespuesta3.isEnabled = true;
+        botonRespuesta4.isEnabled = true;
+    }
+    
+    func mostrarEnBotones(){
+        botonRespuesta1.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 1), for: .normal);
+        botonRespuesta2.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 2), for: .normal);
+        botonRespuesta3.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 3), for: .normal);
+        botonRespuesta4.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 4), for: .normal);
     }
 }
 
--- JuanAPP/Quiz.swift
+++ JuanAPP/Quiz.swift
@@ -11,8 +11,11 @@ import Foundation
 class Quiz {
     
     //MARK: -Propiedades del modelo
-    let preguntasDeportes: Array<String> = ["¿Cuanto es dos mas dos?","Capital de Bukina Faso","¿Como se apellida juan?","¿Por que?","¿Que?"];
-    let respuestasDeportes=["Cuatro","Ouagadougou","Topo","Kha","Na"];
+    let preguntasDeportes: Array<String> = ["¿Qué jugador ha disputado más partidos en la historia de la Champions League?","Después de Cristiano Ronaldo y Messi, ¿Quién ha marcado más goles en la Champions League?","¿En qué fase de Champions anotó Iniesta su famoso gol contra el Chelsea?","¿Cuántos títulos de Champions League tiene el Nottingham Forest?","Este equipo ganó la Final de la Champions League en su estadio"];
+    let respuestasCorrectasDeportes=["Iker Casillas","Raúl González","Semifinales","2", "Inter de Milán"];
+    let respuestasIncorrectasDeportes1=["Xavi Hernández","Ibrahimovic","Cuartos de final","0","Liverpool"];
+    let respuestasIncorrectasDeportes2=["Raúl González","Henry","Final","1","Barcelona"];
+    let respuestasIncorrectasDeportes3=["Gianluigi Buffon","Van Nistelrooy", "Octavos de final","3","Real Madrid"];
     
     var numeroActual = 0;
     var preguntaActual = 0;
@@ -28,16 +31,35 @@ class Quiz {
 //    }
     
     func preguntas(numeroDeCategoria: Int) -> String{
-        numeroActual = aleatoreos();
         switch numeroDeCategoria {
         case 0:
-            return preguntasDeportes[numeroActual];
+            return preguntasDeportes[aleatoreos()];
         default:
             print("Esto no sucedera");
         }
         return "Error faltal";
     }
     
+    func respuestas(numeroDeCategoria: Int,numeroDeBoton: Int) -> String{
+        switch numeroDeCategoria {
+        case 0:
+            switch numeroDeBoton{
+            case 1:
+                return respuestasCorrectasDeportes[preguntaAnterior];
+            case 2:
+                return respuestasIncorrectasDeportes1[preguntaAnterior];
+            case 3:
+                return respuestasIncorrectasDeportes2[preguntaAnterior];
+            case 4:
+                return respuestasIncorrectasDeportes3[preguntaAnterior];
+            default:
+                print("Esto no sucedera");
+            }
+        default:
+            print("Esto no sucedera");
+        }
+        return "Algo";
+    }
 //    func respuesta() -> String{
 //        if (self.preguntaActual == -1){
 //            return "Haga clic en el boton siguiente pregunta";
