dc6326d96c455aceb4989a1b97c2174458e378bf
Leo <leo@Leos-MacBook-Pro.local>
2018-09-24 12:37:56 -0500

    Almost done

--- JuanAPP/JuanAppViewController.swift
+++ JuanAPP/JuanAppViewController.swift
@@ -53,6 +53,8 @@ class JuanAppViewController: UIViewController {
 	@IBOutlet weak var labelMensaje: UILabel!
 	@IBOutlet weak var labelPreguntas: UILabel!
     @IBOutlet weak var labelCategoria: UILabel!
+    @IBOutlet weak var labelAciertos: UILabel!
+    @IBOutlet weak var labelErrores: UILabel!
     
     @IBOutlet weak var botonRespuesta1: UIButton!
     @IBOutlet weak var botonRespuesta2: UIButton!
@@ -61,6 +63,7 @@ class JuanAppViewController: UIViewController {
     
     let modeloQuiz = Quiz();
     var categoria = -1;
+	var respuestasCorrectas:[Bool] = [false,false,false,false] ;
     
     
     
@@ -92,24 +95,23 @@ class JuanAppViewController: UIViewController {
 //    }
     
     @IBAction func eligeCategoria(_ sender: UIButton) {
-        switch sender.tag {
-        case 0:
-            print("1");
-            categoria = 0;
-            labelPreguntas.text = self.modeloQuiz.preguntas(numeroDeCategoria: 0);
+			categoria = sender.tag;
+			let pregunta = self.modeloQuiz.preguntas(numeroDeCategoria: sender.tag);
+		let posicion = pregunta.1;
+			labelPreguntas.text = pregunta.0.texto;
             estiloBoton();
             mostrarEnBotones();
-        case 1:
-            print("2");
-        case 2:
-            print("3");
-        case 3:
-            print("4");
-        case 4:
-            print("5");
-        default:
-            print("Esto no sucedera");
-        }
+
+//		print(pregunta.0.respuestas[posicion[0]]);
+		botonRespuesta1.setTitle(pregunta.0.respuestas[posicion[0]].texto, for: .normal);
+		botonRespuesta2.setTitle(pregunta.0.respuestas[posicion[1]].texto, for: .normal);
+		botonRespuesta3.setTitle(pregunta.0.respuestas[posicion[2]].texto, for: .normal);
+		botonRespuesta4.setTitle(pregunta.0.respuestas[posicion[3]].texto, for: .normal);
+		
+		for k in 0...3{
+			respuestasCorrectas[k] = pregunta.0.respuestas[posicion[k]].correcto;
+		}
+		
         for j in 6...7{
             self.view.subviews[j].isHidden=false;
         }
@@ -127,9 +129,18 @@ class JuanAppViewController: UIViewController {
     }
     
     @IBAction func siguienteCategoria() {
-            categoria = 0;
-            labelPreguntas.text = self.modeloQuiz.preguntas(numeroDeCategoria: categoria);
+		let pregunta = self.modeloQuiz.preguntas(numeroDeCategoria: categoria);
+		let posicion = pregunta.1;
+		labelPreguntas.text = pregunta.0.texto;
+		botonRespuesta1.setTitle(pregunta.0.respuestas[posicion[0]].texto, for: .normal);
+		botonRespuesta2.setTitle(pregunta.0.respuestas[posicion[1]].texto, for: .normal);
+		botonRespuesta3.setTitle(pregunta.0.respuestas[posicion[2]].texto, for: .normal);
+		botonRespuesta4.setTitle(pregunta.0.respuestas[posicion[3]].texto, for: .normal);
             estiloBoton();
+        for k in 0...3{
+            respuestasCorrectas[k] = pregunta.0.respuestas[posicion[k]].correcto;
+        }
+        print(respuestasCorrectas);
         mostrarEnBotones();
         desblockeaBotones();
         botonSiguiente.isEnabled = false;
@@ -154,31 +165,55 @@ class JuanAppViewController: UIViewController {
 		desblockeaBotones();
     }
     
-    @IBAction func BotonRespuestaCorrecta(_ sender: Any) {
-        botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
-        botonAtras.isEnabled = true;
-        botonSiguiente.isEnabled = true;
-        blockeaBotones();
-		labelMensaje.text = "¡Correcto!";
-    }
+//    @IBAction func BotonRespuestaCorrecta(_ sender: Any) {
+//        botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+//        botonAtras.isEnabled = true;
+//        botonSiguiente.isEnabled = true;
+//        blockeaBotones();
+//        labelMensaje.text = "¡Correcto!";
+//    }
 	
 	
 	@IBAction func BotonRespuestaIncorrecta(_ sender: UIButton) {
-		switch sender.tag {
-		case 5:
-		botonRespuesta2.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
-		case 6:
-		botonRespuesta3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
-		case 7:
-		botonRespuesta4.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
-		default:
-			print("Esto no pasara");
-		}
-		botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
-		blockeaBotones();
-		labelMensaje.text = "¡Incorrecto!";
-		botonAtras.isEnabled = true;
-		botonSiguiente.isEnabled = true;
+        switch sender.tag {
+        case 5:
+            if (respuestasCorrectas[0]){
+                botonRespuesta1.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+                    labelMensaje.text = "¡Correcto!";
+            }else{
+                botonRespuesta1.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Incorrecto!";
+            }
+        case 6:
+            if (respuestasCorrectas[1]){
+                botonRespuesta2.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Correcto!";
+            }else{
+                botonRespuesta2.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Incorrecto!";
+            }
+        case 7:
+            if (respuestasCorrectas[2]){
+                botonRespuesta3.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Correcto!";
+            }else{
+                botonRespuesta3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Incorrecto!";
+            }
+        case 8:
+            if (respuestasCorrectas[3]){
+                botonRespuesta4.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Correcto!";
+            }else{
+                botonRespuesta4.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1);
+                labelMensaje.text = "¡Incorrecto!";
+            }
+        default:
+            print("Esto no sucedera");
+        }
+        botonAtras.isEnabled = true;
+        botonSiguiente.isEnabled = true;
+        blockeaBotones();
 	}
 	
     
@@ -207,11 +242,16 @@ class JuanAppViewController: UIViewController {
         botonRespuesta4.isEnabled = true;
     }
     
+    @IBAction func reiniciarMarcador() {
+        labelAciertos.text = "-";
+        labelErrores.text = "-";
+    }
+    
     func mostrarEnBotones(){
-        botonRespuesta1.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 1), for: .normal);
-        botonRespuesta2.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 2), for: .normal);
-        botonRespuesta3.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 3), for: .normal);
-        botonRespuesta4.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 4), for: .normal);
+//        botonRespuesta1.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 1), for: .normal);
+//        botonRespuesta2.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 2), for: .normal);
+//        botonRespuesta3.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 3), for: .normal);
+//        botonRespuesta4.setTitle(self.modeloQuiz.respuestas(numeroDeCategoria: categoria, numeroDeBoton: 4), for: .normal);
     }
 }
 
